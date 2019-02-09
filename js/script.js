6213f3a414b56eaa6af7d75285ff0fa6548a390a
juju <julie.dejeanty@gmail.com>
2019-02-09 23:21:53 +0100

    modif css

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 $(window).on("scroll", function(){
     let positionY = $(this).scrollTop();
     console.log(positionY);
-})
 
-$(window).on("scroll", function(){
-    let positionY = $(this).scrollTop();
-
-    if(positionY === 796){
-        $("#scrolltotop").css("display","flex")
+    if(positionY > 296){
+        $("#scrolltotop").css("opacity", 1);
     }
-    else{
-        $("#scrolltotop").css("display","none")
+    else if(positionY < 296){
+        $("#scrolltotop").css("opacity", 0);
     }
 
+    if(positionY > 100){
+        $("nav").css("height","50px")
+        $("nav").css("height","50px")
+    }
 })
+
+
+//lorsque position y est supérieur à 100
+    // réduire height de la nav
+    // position fixe en haut de la nav
